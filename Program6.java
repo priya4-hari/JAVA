8dff9f332ca6a76259f14729cbc46c744ec1cae4
priya4-hari <haripriyaandavar2001@gmail.com>
2024-05-24 22:13:34 +0530

    String arg print

---
+++ Program6.java
@@ -0,0 +1,14 @@
+class Program6
+{
+	public static void main(String[] args)
+	{
+			if(args[0]!="\0" || args.length!=0)
+			{
+			System.out.println(args[0]+","+args[1]);
+			}
+			else
+			{
+			System.out.println("No values");
+			}
+	}
+}
