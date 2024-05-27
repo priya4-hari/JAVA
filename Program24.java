81c23403b1f1b0eeabebddc0d9efdf4d200b90df
priya4-hari <haripriyaandavar2001@gmail.com>
2024-05-27 23:43:14 +0530

    find Average

---
+++ Program24.java
@@ -0,0 +1,18 @@
+class Program24
+{
+	public static void main(String[] args)
+	{
+		int a[]={1,2,3,4,5};
+		int s=0;
+		float avg;
+		for(int i=0;i<a.length;i++)
+		{
+			s+=a[i];
+		}
+		avg=s/(a.length);
+		System.out.println(s);
+		System.out.println(avg);
+		}
+	}
+		
+			
