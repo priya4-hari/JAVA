cb2752aad441f16e0956e73dcec188e0341eb68b
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-06 21:46:55 +0530

    Create CountNumberOfWords1.java

---
+++ CountNumberOfWords1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Count
+{
+  public static void main(String[] args)
+  {
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+    String[] p=s.split(" ");
+    System.out.println(p.length);
+  }
+}
