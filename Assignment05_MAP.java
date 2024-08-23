712ddc31d6ccb82fde195df6cb61dbc4f0e8f7a6
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-23 22:32:01 +0530

    Update Assignment05_MAP.java

--- Assignment05_MAP.java
+++ Assignment05_MAP.java
@@ -7,7 +7,7 @@ Japan                                   Tokyo
 2. Develop a method getCapital(String CountryName) which returns the capital for the country passed, from the Map M1 created in step 1.
 3. Develop a method getCountry(String capitalName) which returns the country for the capital name, passed from the Map M1 created in step 1.
 4. Develop a method which iterates through the map M1 and creates another map M2 with Capital as the key and value as Country and returns the Map M2.
-Key – Capital                    Value – Country
+Key â€“ Capital                    Value â€“ Country
 Delhi                                 India
 Tokyo                                 Japan
 5. Develop a method which iterates through the map M1 and creates an ArrayList with all the Country names stored as keys. This method should return the ArrayList.
@@ -80,7 +80,7 @@ class Country {
 public class Assignment05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Country country = new Country();
 
 		country.saveCountryCapital("India", "Delhi");
