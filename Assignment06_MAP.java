fdd17e98f566872d4a2cbafb6e9fc95a97e50bcc
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-24 21:20:50 +0530

    Update Assignment06_MAP.java

--- Assignment06_MAP.java
+++ Assignment06_MAP.java
@@ -1,4 +1,4 @@
-//Implement the assignment 1 using HashTable
+
 /*Assignment 1:
 1. Develop a java class with a instance variable M1 (HashMap)  create a method saveCountryCapital(String CountryName, String capital) , the method should add the passed country and capital as key/value in the map M1 and return the Map (M1).
 Key- Country                          Value - Capital
@@ -7,7 +7,7 @@ Japan                                   Tokyo
 2. Develop a method getCapital(String CountryName) which returns the capital for the country passed, from the Map M1 created in step 1.
 3. Develop a method getCountry(String capitalName) which returns the country for the capital name, passed from the Map M1 created in step 1.
 4. Develop a method which iterates through the map M1 and creates another map M2 with Capital as the key and value as Country and returns the Map M2.
-Key – Capital                    Value – Country
+Key â€“ Capital                    Value â€“ Country
 Delhi                               	India
 Tokyo                            		Japan
 5. Develop a method which iterates through the map M1 and creates an ArrayList with all the Country names stored as keys. This method should return the ArrayList.
