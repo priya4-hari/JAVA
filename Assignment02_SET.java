9b88b85cb23ac844ebe16a7da9847796f66977d7
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-25 22:13:42 +0530

    Update Assignment02_SET.java

--- Assignment02_SET.java
+++ Assignment02_SET.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 public class Assignment02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		HashSet<String> set = new HashSet<>();
 		
 		set.add("Alice");
