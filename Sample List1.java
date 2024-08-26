03c084c284c15a0c8cf59bc437970099d68aac09
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-26 21:51:33 +0530

    Create Sample List1.java

---
+++ Sample List1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SampleList1
+{
+  public static void main(String[] args)
+  {
+    List<String>MyList=new ArrayList<>();
+    MyList.add("Hello");
+    MyList.add("World");
+    System.out.println(MyList);
+  }
+}
