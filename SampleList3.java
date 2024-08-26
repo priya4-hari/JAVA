a31886f91d468a11f2825970be816feed9ec1e97
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-26 21:57:31 +0530

    Create SampleList3.java

---
+++ SampleList3.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SampleList3
+{
+  public static void main(String[] args)
+  {
+    List<Integer>MyList=new ArrayList<>();
+    MyList.add(10);
+    MyList.add(20);
+    MyList.add(1,15);
+    System.out.println(MyList);
+  }
+}
