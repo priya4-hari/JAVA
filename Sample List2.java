de7fc4cfd5852a489224edfdb18ed03c3e6c3827
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-26 21:54:30 +0530

    Create Sample List2.java

---
+++ Sample List2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SampleList2
+{
+  public static void main(String[] args)
+  {
+    List<Integer>MyList=new ArrayList<>();
+    MyList.add(10);
+    MyList.add(15);
+    MyList.add(20);
+    System.out.println(MyList);
+  }
+}
