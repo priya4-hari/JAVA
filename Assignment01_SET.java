2c69e91c0b10a407399807fca4292f5d2edd49c7
Haripriya Andavar <60725880+priya4-hari@users.noreply.github.com>
2024-08-27 21:47:39 +0530

    Update Assignment01_SET.java

--- Assignment01_SET.java
+++ Assignment01_SET.java
@@ -1,7 +1,7 @@
 /*Develop a java class with a instance variable H1 (HashSet)  add a method saveCountryNames(String CountryName) , the method should add the passed country to a HashSet (H1) and return the added HashSet(H1).
 Develop a method getCountry(String CountryName) which iterates through the HashSet and returns the country if exist else return null. 
 NOTE: You can test the methods using a main method.
-
+*/
 
 import java.util.HashSet;
 import java.util.Iterator;
