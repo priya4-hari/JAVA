c19445c3e6f666b27f66a5ad775232769ea33d88
priya4-hari <haripriyaandavar2001@gmail.com>
2024-11-14 21:43:53 +0530

    Word Count with Method

---
+++ StringWordCountWithMethod.java
@@ -0,0 +1,16 @@
+package com.test.org;
+
+import java.util.Scanner;
+
+public class StringWordCountWithMethod {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Enter string:");
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		String[] arr = str.split(" ");
+		System.out.println(arr.length);
+	}
+
+}
